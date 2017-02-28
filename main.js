152a00137a7109adf0f2a5b2f1eb7e294d80bce9
gTopa <gtopa@stuy.edu>
2017-02-28 15:15:07 -0500

    work

--- main.js
+++ main.js
@@ -4,19 +4,21 @@ var y=-1
 var makeCircle = function(e) {
     var mouseX = e.offsetX;
     var mouseY = e.offsetY;
-    var c = document.createElementNS("https://www.w3.org/2000/svg","circle");
+    var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
     c.setAttribute("cx",mouseX);
     c.setAttribute("cy",mouseY);
-    c.setAttribute("r", "100");
+    c.setAttribute("r", "20");
     c.setAttribute("fill","yellow");
     document.getElementById("vimage").appendChild(c);
     console.log("hi");    
     if(x!=-1&&y!=-1){
-	var a = document.createElementNS("https://www.w3.org/2000/svg","line");
+	var a = document.createElementNS("http://www.w3.org/2000/svg","line");
 	a.setAttribute("x1",x);
 	a.setAttribute("y1",y);
 	a.setAttribute("x2",mouseX);
 	a.setAttribute("y2",mouseY);
+	a.setAttribute("stroke-width", "2");
+	a.setAttribute("stroke", "black");
 	document.getElementById("vimage").appendChild(a);
 	console.log("hi");
     };
